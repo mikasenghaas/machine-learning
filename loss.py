5c1149cd5a29f35d388b2a57169ce0a0e39967e2
Jonas-Mika Senghaas <accounts@jonassenghaas.de>
2021-10-27 20:47:59 +0200

    Added loss.py

---
+++ loss.py
@@ -0,0 +1,20 @@
+# script that contains commonly used loss-function in machine learning
+import numpy as np
+
+def mse(y, p):
+    return 1 / len(y) * sum((y - p)**2)
+
+def se(y, p):
+    return sum((y - p)**2)
+
+def mae(y, p):
+    return 1 / len(y) * sum(np.abs(y-p))
+
+def zero_one_loss(y, p):
+    return np.sum(y != p)
+
+def binary_cross_entropy(y, p):
+    return - (1 / len(y)) * np.sum(y * np.log(p) + ((1+y) * np.log(1-p)))
+
+def multiclass_cross_entropy(y, p):
+    return - (1 / y.shape[0]) * np.sum(y * np.log(p))
