4eb62b6db0ba3ad45e952768a2aae62ea3cffbaa
Jonas-Mika Senghaas <accounts@jonassenghaas.de>
2021-11-18 00:02:50 +0100

    Improved plot_1d_decision_regions and plot_2d_decision_regions functions

--- eduml/plotting/__init__.py
+++ eduml/plotting/__init__.py
@@ -3,6 +3,8 @@ The :mod:`eduml.plotting` module includes basic plotting functionality
 for machine learning applications.
 """
 
+from ._classification import plot_1d_decision_regions
+from ._classification import plot_2d_decision_regions
 from ._classification import plot_decision_regions
 
 __all__ = [
--- eduml/plotting/_classification.py
+++ eduml/plotting/_classification.py
@@ -1,44 +1,87 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
-def plot_decision_regions(x, y, model, mesh_size=0.01, title='Unnamed'):
-    redish = '#d73027'
-    orangeish = '#fc8d59'
-    yellowish = '#fee090'
-    blueish = '#4575b4'
+from ..utils import COLORS
 
-    colormap = np.array([blueish, redish])
-    labels = np.array(['Class 1', 'Class 2'])
+def plot_1d_decision_regions(x, y, model, mesh_size=0.01, figsize=(8,6), title='unnamed'):
+    assert len(x.shape) == 1, 'Feature Space must be 1-dimensional'
+    n, p  = len(y), 1
+    k = len(np.unique(y))
 
+    # define styling properties
+    colormap = np.array(COLORS[:k])
+    mapping = {trg: i for i, trg in enumerate(np.unique(y))}
+    labels = np.array([f'Class {i+1}' for i in range(k)])
+
+    fig, ax = plt.subplots(figsize=figsize)
+
+    # meshgrid plotting for decision boundary
+    PAD = 0.1 # relative padding
+    x_min, x_max = x.min()*(1-PAD), x.max()*(1+PAD)
+    x_range = np.arange(x_min, x_max, mesh_size)
+    y_min, y_max = -5*(1-PAD), 5*(1+PAD)
+    y_range = np.arange(y_min, y_max, mesh_size)
+
+    xx0, xx1 = np.meshgrid(x_range, y_range)
+    xx = np.reshape(np.stack((xx0.ravel(),xx1.ravel()),axis=1),(-1,2))
+
+    pred_y = [mapping[trg] for trg in model.predict(xx[:,0].reshape(-1, 1))]
+    ax.scatter(xx[:, 0], xx[:, 1], c=colormap[pred_y], alpha=0.3)
+
+    for k, color, label in zip(np.unique(y), colormap, labels):
+        ax.scatter(x[y==k], np.ones(sum(y==k)), c=color, s=20, edgecolor='black', linewidth=1, label=label)
+
+    ax.set_title(title, fontweight='bold')
+    ax.set_xlabel('$X_1$')
+    ax.legend(loc='best')
+
+    return fig
+
+def plot_2d_decision_regions(x, y, model, mesh_size=0.01, figsize=(8, 6), title='unnamed'):
+    """
+    Function to plot two dimensional decision regions
+    """
+    # global properties of data
+    assert len(x.shape) == 2, 'Feature Space must be 2-dimensional'
+
+    n, p = x.shape
+    k = len(np.unique(y))
+
+    # define styling properties
+    colormap = np.array(COLORS[:k])
+    mapping = {trg: i for i, trg in enumerate(np.unique(y))}
+    labels = np.array([f'Class {i+1}' for i in range(k)])
+
+    # prepare data
     x0 = x[:, 0]
     x1 = x[:, 1]
-    y = np.array(y).astype(int)
 
-    fig, ax = plt.subplots(figsize=(8,6), dpi=150)
+    y = np.array([mapping[trg] for trg in y])
+
+    fig, ax = plt.subplots(figsize=figsize)
 
     # meshgrid plotting for decision boundary
     PAD = 0.1 # relative padding
     x0_min, x0_max = x0.min()*(1-PAD), x0.max()*(1+PAD)
+    x0_range = np.arange(x0_min, x0_max, mesh_size)
     x1_min, x1_max = x1.min()*(1-PAD), x1.max()*(1+PAD)
-    x0_axis_range = np.arange(x0_min,x0_max, mesh_size)
-    x1_axis_range = np.arange(x1_min,x1_max, mesh_size)
+    x1_range = np.arange(x1_min, x1_max, mesh_size)
 
-    xx0, xx1 = np.meshgrid(x0_axis_range, x1_axis_range)
+    xx0, xx1 = np.meshgrid(x0_range, x1_range)
     xx = np.reshape(np.stack((xx0.ravel(),xx1.ravel()),axis=1),(-1,2))
 
-    pred_y = model.predict(xx).astype(int).reshape(-1,)
+    pred_y = [mapping[trg] for trg in model.predict(xx)]
+    ax.scatter(xx[:, 0], xx[:, 1], c=colormap[pred_y], alpha=0.3)
 
-    if len(model.predict_proba(xx)) > 0:
-        pred_s = model.predict_proba(xx) 
-        ax.scatter(xx[:, 0], xx[:, 1], c=colormap[pred_y], s=50*pred_s**3, alpha=0.1, linewidths=0,)
-    else:
-        ax.scatter(xx[:, 0], xx[:, 1], c=colormap[pred_y], alpha=0.3, linewidths=0,)
 
-    ax.contour(x0_axis_range, x1_axis_range,
+    """
+    ax.contour(x0_range, x1_range,
                np.reshape(pred_y,(xx0.shape[0],-1)),
                levels=len(np.unique(y))-1, linewidths=1,
                colors=colormap[np.unique(pred_y)])
+    """
 
+    # scatter training points
     for k, color, label in zip(np.unique(y), colormap, labels):
         ax.scatter(x0[y==k], x1[y==k], c=color, s=20, edgecolor='black', linewidth=1, label=label)
 
@@ -49,8 +92,5 @@ def plot_decision_regions(x, y, model, mesh_size=0.01, title='Unnamed'):
 
     return fig
 
-def test():
+def plot_decision_regions():
     pass
-
-if __name__ == '__main__':
-    test()
