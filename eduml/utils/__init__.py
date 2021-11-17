473a7292c766d1e308219836360dec7b0ad738e4
Jonas-Mika Senghaas <accounts@jonassenghaas.de>
2021-11-18 00:19:47 +0100

    Added validators for input feature matrices and target vectors

--- eduml/utils/__init__.py
+++ eduml/utils/__init__.py
@@ -4,9 +4,13 @@ when dealing with machine learning algorithms
 """
 
 from ._validate import check_consistent_length
+from ._validate import validate_feature_matrix
+from ._validate import validate_target_vector
 from ._colors import COLORS
 
 __all__ = [
-        'check_consistent_length'
+        'check_consistent_length',
+        'validate_feature_matrix',
+        'validate_target_vector',
         'COLORS'
         ]
