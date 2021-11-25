d6efbfe688b0c449ffb9ed87c97188fe0f45c0e6
Jonas-Mika Senghaas <accounts@jonassenghaas.de>
2021-11-25 20:30:01 +0100

    Implemented plot_1d_regression to plot on generic plt.Axes object as function input

--- eduml/plotting/_regression.py
+++ eduml/plotting/_regression.py
@@ -1,8 +1,9 @@
 from matplotlib import pyplot as plt
 import numpy as np 
 
-def plot_1d_regression(X, y, model, figsize=(8,6), title='unnamed'):
-    fig, ax = plt.subplots(figsize=figsize)
+def plot_1d_regression(X, y, model, title='unnamed', ax=None):
+    if ax==None:
+        fig, ax = plt.subplots(figsize=(8, 6))
 
     ax.scatter(X, y, c='b', label='$X_1$');
     x_range = np.linspace(min(X), max(X), 1000)
@@ -12,4 +13,4 @@ def plot_1d_regression(X, y, model, figsize=(8,6), title='unnamed'):
     ax.set_ylabel('$y$');
     ax.grid()
 
-    return fig
+    return ax 
